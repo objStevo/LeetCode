f6d7ad9b4cf7821c913ead48cf4dd545dac9293f
Steve Chalco <stevechalco@Steves-MacBook-Pro.local>
2022-02-15 21:53:19 -0800

    linkedlist prototype learnings

---
+++ linkedList.js
@@ -0,0 +1,65 @@
+var Node = function (val, next){
+    this.value = val;
+    this.next = next instanceof Node ? next : null;
+}
+
+Node.prototype.get = function(){
+    return this.value;
+}
+Node.prototype.set = function (value){
+
+};
+var MyLinkedList = function() {
+    return true;
+};
+
+/** 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+    
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    
+};
+
+/** 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    
+};
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
