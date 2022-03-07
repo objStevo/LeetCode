3859a6e0d33571af8349ab4ab302d800227f60f5
Steve Chalco <hschalco@gmail.com>
2022-03-07 07:48:14 -0800

    add at tail and add at head function

--- linkedList.js
+++ linkedList.js
@@ -1,56 +1,67 @@
-var Nodes = function (val){
-    this.value = val;
-    this.next = null;
-}
-
-var MyLinkedList = function() {
-    this.head = null;
+var Nodes = function (val) {
+  this.value = val;
+  this.next = null;
 };
 
-/** 
+var MyLinkedList = function () {
+  this.head = null;
+};
+
+/**
  * @param {number} index
  * @return {number}
  */
-MyLinkedList.prototype.get = function(index) {
-    
+MyLinkedList.prototype.get = function (index) {
+  if (!this.head) return -1;
+  let count = 0,
+    current = this.head;
+
+  while (current) {
+    if (count === index) return current.value;
+    count++;
+    current = current.next;
+  }
+  return -1;
 };
 
-/** 
+/**
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtHead = function(val) {
-    const prevHead = this.head;
-    this.head = new Nodes(val);
-    this.head.next = prevHead ? prevHead : null;
+MyLinkedList.prototype.addAtHead = function (val) {
+  const prevHead = this.head;
+  this.head = new Nodes(val);
+  this.head.next = prevHead ? prevHead : null;
 };
 
-/** 
+/**
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtTail = function(val) {
-    
+MyLinkedList.prototype.addAtTail = function (val) {
+  if (!this.head) this.head = new Nodes(val);
+
+  let current = this.head;
+  while(current.next){
+      current = current.next;
+  }
+  current.next = new Nodes(val);
 };
 
-/** 
- * @param {number} index 
+/**
+ * @param {number} index
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtIndex = function(index, val) {
-    
-};
+MyLinkedList.prototype.addAtIndex = function (index, val) {};
 
-/** 
+/**
  * @param {number} index
  * @return {void}
  */
-MyLinkedList.prototype.deleteAtIndex = function(index) {
-    
-};
+MyLinkedList.prototype.deleteAtIndex = function (index) {};
 
-/** 
+/**
  * Your MyLinkedList object will be instantiated and called as such:
  * var obj = new MyLinkedList()
  * var param_1 = obj.get(index)
