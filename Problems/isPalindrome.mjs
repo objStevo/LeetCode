c51f6f78e94627b9b17cc37530ffd8016539951c
Steve Chalco <hschalco@gmail.com>
2023-05-08 22:22:02 -0700

    ispalidrome

---
+++ Problems/isPalindrome.mjs
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ **/
+
+export const isPalindrome = (x) => {
+  const xString = x.toString();
+  const center = Math.floor(xString.length / 2);
+  let result = true;
+
+  if (x < 0) return false;
+  if (0 <= x && x <= 9) return true;
+
+  for (let i = 0; i < center && result === true; i++) {
+    if (xString[i] !== xString[xString.length - 1 - i]) result = false;
+  }
+
+  return result;
+};
+
+console.log(isPalindrome(131));
+console.log(isPalindrome(1312341));
+console.log(isPalindrome(-131));
+console.log(isPalindrome(0));
+console.log(isPalindrome(145632376541));
